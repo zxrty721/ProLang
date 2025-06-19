ae7ab9d4815bea488d461a1290b4c9d6cd4bf479
zxrty721 <zxrty721@gmail.com>
2025-06-19 19:52:32 +0700

    Implement manual base URL for image paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  // **ต้องเป็นชื่อผู้ใช้ GitHub ของคุณ**
   site: 'https://Tew0979271981.github.io',
-
-  // **ต้องเป็นชื่อ Repository ของคุณ**
   base: '/ProLang',
-
   output: 'static',
-
-  assetsPrefix: '/ProLang',
-
-  integrations: [react()]
+  // assetsPrefix: '/ProLang', // <<< ลบบรรทัดนี้ออกไป!
+  integrations: [
+    react(),
+  ],
 });
