b771c6c552ffa0880361de5a1cde9f52524de2a7
zxrty721 <zxrty721@gmail.com>
2025-06-19 18:30:03 +0700

    Add Astro React integration to fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Tew0979271981.github.io', // **ต้องเป็นชื่อผู้ใช้ GitHub ของคุณ**
-  base: '/ProLang', // **ต้องเป็นชื่อ Repository ของคุณ**
+  // **ต้องเป็นชื่อผู้ใช้ GitHub ของคุณ**
+  site: 'https://Tew0979271981.github.io',
+
+  // **ต้องเป็นชื่อ Repository ของคุณ**
+  base: '/ProLang',
+
+  integrations: [react()]
 });
